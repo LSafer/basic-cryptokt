82c10bae6c45b17a3bef48c44e0428c65b4188d6
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2024-07-02 11:58:48 +0300

    feat: md5

---
+++ src/jsMain/kotlin/md5.js.kt
@@ -0,0 +1,17 @@
+package net.lsafer.crypto
+
+actual fun ByteArray.md5(): ByteArray {
+    TODO("Not yet implemented")
+}
+
+actual fun String.md5(): ByteArray {
+    TODO("Not yet implemented")
+}
+
+actual fun ByteArray.md5ToString(): String {
+    TODO("Not yet implemented")
+}
+
+actual fun String.md5ToString(): String {
+    TODO("Not yet implemented")
+}
---
+++ src/jvmMain/kotlin/md5.jvm.kt
@@ -0,0 +1,20 @@
+package net.lsafer.crypto
+
+import java.security.MessageDigest
+
+actual fun ByteArray.md5(): ByteArray {
+    val digest = MessageDigest.getInstance("MD5")
+    return digest.digest(this)
+}
+
+actual fun String.md5(): ByteArray {
+    return encodeToByteArray().md5()
+}
+
+actual fun ByteArray.md5ToString(): String {
+    return md5().encodeHex()
+}
+
+actual fun String.md5ToString(): String {
+    return md5().encodeHex()
+}
